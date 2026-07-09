f34c7064c61e67cb6190517a2f823c543a874046
ER-RAHUL-RATHOD <rahul.rathod@atmamalikdhyanpeeth.com>
2026-07-09 23:02:56 +0530

    Update Supabase URL in supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ybuzxovumbedczsvokbv.supabase.co/rest/v1/'
+const supabaseUrl = 'https://ybuzxovumbedczsvokbv.supabase'
 const supabaseKey = 'eyeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlidXp4b3Z1bWJlZGN6c3Zva2J2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2MTAzMDgsImV4cCI6MjA5OTE4NjMwOH0.GhIYPhTM6KCSkNjnS6u_i6CKYzKq0wkzJ2W-N8_bCPo'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
